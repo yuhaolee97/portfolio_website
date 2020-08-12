c2e81ad8b40a8868b7d4be1b25164ae9dfa8a1e1
yuhaolee97 <yuhaolee97@gmail.com>
2020-08-12 11:30:47 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio_website/precache-manifest.5f1c0986d35f522317aaf9b46b082412.js"
+  "/portfolio_website/precache-manifest.1a7e8613c310f6daea6a4daaab48756e.js"
 );
 
 self.addEventListener('message', (event) => {
